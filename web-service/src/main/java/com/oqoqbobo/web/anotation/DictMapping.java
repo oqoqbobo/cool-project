e05e7c8ebad07cb6b955f24b41cb81a1f259e21a
oqoqbobo <156905832@qq.com>
2022-05-24 09:51:27 +0800

    other

--- web-service/src/main/java/com/oqoqbobo/web/anotation/DictMapping.java
+++ web-service/src/main/java/com/oqoqbobo/web/anotation/DictMapping.java
@@ -17,12 +17,4 @@ public @interface DictMapping {
     //字典值来源字段
     String sourceField() default "";
 
-    //目标字符串拼接，如值为200 targetJoinFiexStr为：A 则目标值为:200A
-    String targetJoinFiexStr() default "";
-
-    //是否校验值为254异常、255无效
-    boolean isValidateError() default false;
-
-    //校验数据长度 1-254异常、255无效,2-65534异常、65535无效, 3-4094异常、4095异常, 4-4294967294异常、4294967295异常
-    int validateErrorLenth() default 0;
 }
